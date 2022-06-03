0e5544fcb9503c350d15d80153feb8e08fb0625c
bennywu2019 <benny.wu.new@gmail.com>
2022-06-03 12:23:48 -0400

    Initial commit

---
+++ Character.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Character here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Character extends Entity
+{
+    /**
+     * Act - do whatever the Character wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
