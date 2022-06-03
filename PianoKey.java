fd64bbd69d2be62e2f51b43a88441ff13439ee1e
zks10 <kevinzhu20041010@gmail.com>
2022-06-03 19:04:08 -0400

    Finished the piano features

---
+++ PianoKey.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PianoKey here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PianoKey extends Actor
+{
+    private String key;
+    private String sound;
+    private String up;
+    private String down;
+    boolean isPressed;
+    /**
+     * Act - do whatever the PianoKey wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public PianoKey(String key, String sound, String up, String down){
+        setImage(up);
+        this.key=key;
+        this.sound=sound;
+        this.up=up;
+        this.down=down;
+    }
+    public void act()
+    {
+        keyPressed();
+    }
+   
+    public void keyPressed(){
+        if(Greenfoot.isKeyDown(key)&&!isPressed){
+            setImage(down);
+            playSound();
+            isPressed=true;
+        }
+        if(!Greenfoot.isKeyDown(key)&&isPressed){
+            setImage(up);
+            isPressed=false;
+        }
+    }
+    
+    public void playSound(){
+        Greenfoot.playSound(sound);
+    }
+}
