b3d48ea291eacd6cb428fa8ad0055d834f5f9ad1
bennywu2019 <benny.wu.new@gmail.com>
2022-06-03 17:03:29 -0400

    Added wave effect

--- Grid.java
+++ Grid.java
@@ -10,14 +10,41 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Grid extends Actor
 {
+    enum GridType {
+        HOUSE,
+        GRASS,
+        WATER
+    }
     
     private int x, y;
+    private GridType type;
     public Grid(int x, int y){
+        this.x = x; this.y = y;
         GreenfootImage sq = new GreenfootImage("Square.png");
         sq.scale(50, 50);
         setImage(sq);
-        
     }
+    
+    public Grid(GreenfootImage image, int x, int y){
+        this.x = x; this.y = y;
+        image.scale(50, 50);
+        setImage(image);
+    }
+    
+    /**
+     * Sets the grid type (enum) of this grid object
+     */
+    public void setType(GridType type){
+        this.type = type;
+    }
+    
+    /**
+     * Gets the grid type (enum) of this grid object
+     */
+    public GridType getType(){
+        return this.type;
+    }
+    
     
     /**
      * Act - do whatever the Grid wants to do. This method is called whenever
--- MyWorld.java
+++ MyWorld.java
@@ -14,19 +14,50 @@ public class MyWorld extends World
      * 
      */
     private final static int WIDTH = 1000, HEIGHT = 700;
+    private Grid[][] grids;
+
+    private int actCounter, test;
     public MyWorld()
     {    
         // Create a new world with 1000x700 cells with a cell size of 1x1 pixels.
-        super(WIDTH, HEIGHT, 1); 
+        // Allow infinite grid to ensure flexibility. However, ensure that objects can not go outside easily
+        super(WIDTH, HEIGHT, 1, false); 
+        grids = new Grid[WIDTH / 50 + 2][HEIGHT / 50 + 2]; // Add 2 grids on each axis for buffer space
         Player bunny = new Player(1);
         addObject(bunny, 100, 100);
         
         setPaintOrder(Entity.class, Grid.class);
-        for (int i = 0; i < WIDTH / 50; i++){
-            for (int j = 0; j < HEIGHT / 50; j++){
-                addObject(new Grid(i * 50, j * 50), i * 50 + 25, j * 50 + 25);
+        for (int i = 0; i < WIDTH / 50 + 2; i++){
+            for (int j = 0; j < HEIGHT / 50 + 2; j++){
+                Grid cur = new Grid(new GreenfootImage("Water" + ((i + j) % 4 + 1) + ".png") , i * 50, j * 50);
+                cur.setType(Grid.GridType.WATER);
+                grids[i][j] = cur;
+                addObject(grids[i][j], i * 50 - 25, j * 50 - 25);
             }
         }
+    }
+    
+    public void act(){
+        // Call the wave effect every 50 acts
+        if (actCounter % 50 == 0) waveEffect();
+        
+        actCounter ++;
+    }
+    
+    public void waveEffect(){
+    
+        
+        for (int i = 0; i < WIDTH / 50 + 2; i++){
+            
+            for (int j = 0; j < HEIGHT / 50 + 2; j++){
+                if (grids[i][j].getType() != Grid.GridType.WATER) continue;
+                int sign = test == 0 ? 1 : - 1;
+                int add = (j % 2 == 0 ? 2 : -2) * sign;
+                grids[i][j].setLocation(grids[i][j].getX() + add, j * 50 -25);
+            }
+        }
+        test ^= 1;
+        
     }
     
 }
